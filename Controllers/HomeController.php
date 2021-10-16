a50136677648697cab408091574df287d254046e
ManuGavioli <juangavioli@gmail.com>
2021-10-16 12:04:30 -0300

    primer commit

---
+++ Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+    namespace Controllers;
+
+    class HomeController
+    {
+
+        public function Index(){
+            require_once(VIEWS_PATH.".php"); // aca hay que mandarlo a la pantalla de login
+        }
+
+        public function ShowAlumnView(){
+            require_once(VIEWS_PATH."validate-session.php");
+            require_once(VIEWS_PATH.".php"); //aca hay que mostrar las cosas que van despues del login
+        }
+
+        public function Login(){
+            if(true)// aca se corrobora que el mail existe en el sistema
+            {
+                $_SESSION["loggedUser"] = true; // igualarlo al mail
+                $this->ShowAlumnView();
+            }
+            else
+                echo "<script> confirm('El mail ingresado no es correcto, vuelva a intentarlo');</script>";
+                $this->Index();
+        }
+        
+        public function Logout(){
+            session_destroy();
+
+            $this->Index();
+        }
+    }
+?>
